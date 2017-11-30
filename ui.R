31291caf70e6ec72a82168d3d688671462a48421
brendan carlquist <carlquistb@gmail.com>
2017-11-29 21:30:10 -0800

    added structure fr contributors to work with.

---
+++ ui.R
@@ -0,0 +1,50 @@
+
+#Shiny App UI
+
+#libraries
+  library(shiny)
+  #TODO: Each of us will need to install this, so that it works on our machines.
+  #install.packages("shinythemes")
+  library(shinythemes)
+
+#source files for each contributor
+  source("contributorScripts/brendan.R")
+
+shinyUI(
+  
+  navbarPage("birth rate analysis", #theme = "themeWeChoose",
+             
+    ####example tab####
+    tabPanel("example",
+      sidebarLayout(
+        sidebarPanel(
+          textOutput("sampleSidebarOutput")
+        ),
+        mainPanel(
+          textOutput("sampleMainPanelOutput")
+        )
+      )
+    ),
+    
+    ####map tab####
+    tabPanel("world map explorer"
+      
+    ),
+    
+    ####markdown####
+    navbarMenu("About",
+      tabPanel("Using this app",
+        includeMarkdown("md/instructions.md")
+      ),
+      tabPanel("Contributions",
+        includeMarkdown("md/contrib.md")
+      ),
+      tabPanel("Sources",
+        includeMarkdown("md/sources.md")
+      ),
+      tabPanel("Conclusions and Discussions",
+        includeMarkdown("md/conclusions.md")
+      )
+    )
+  )
+)
